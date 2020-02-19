19d003442204c66ef7a4e3cff7c632dea4b55dd2
Jack Williams <j.williams@ou.edu>
2020-02-19 00:18:06 -0600

    uncommented important lines

--- R/batch.R
+++ R/batch.R
@@ -23,13 +23,13 @@ run_GSC = function(filename=GSC::filename, c = 100, f = 200, em = 100,
 
   b = GSC::compute_diss(a$customer.df, a$facility.df)
 
-  #reticulate::source_python(paste(system.file(package="GSC"), "partition.py", sep="/"))
+  reticulate::source_python(paste(system.file(package="GSC"), "partition.py", sep="/"))
 
-  #c = partition(b$c, as.vector(t(b$f)), as.vector(t(b$s)), as.vector(b$d), 3, sol_lim, time_lim, optim_lim, emphasis)
+  c = partition(b$c, as.vector(t(b$f)), as.vector(t(b$s)), as.vector(b$d), 3, sol_lim, time_lim, optim_lim, emphasis)
 
   to_ret = list('cust.loc'=a$customer.df, 'fac.loc'=a$facility.df,
-                #'connect' = c$connect, 'open' = c$open,
-                #'cost'=c$cost,
+                'connect' = c$connect, 'open' = c$open,
+                'cost'=c$cost,
                 'em.cost' = package$EMISSIONS_PRICE_TON,
                 'cust.cost' = b$c, 'fac.cost' = b$f)
 
